991b436a871332b169f600d1752bd47d8ebf11c5
shri-prakhar <23f3000621@es.study.iitm.ac.in>
2025-12-05 17:48:02 +0530

    initial commit

--- backend/src/auth/handlers.rs
+++ backend/src/auth/handlers.rs
@@ -1,1 +1,27 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
 
+#[derive(Debug, Deserialize, Validate)]
+pub struct SignUpDto {
+    #[validate(email)]
+    pub email: String,
+    #[validate(length(min = 8))]
+    pub password: String,
+    pub display_name: Option<String>,
+    pub device_name: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct LoginDto {
+    #[validate(email)]
+    pub email: String,
+    #[validate(length(min = 8))]
+    pub password: String,
+    pub device_name: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+struct AccessTokenResponse {
+    access_token: String,
+    expires_in: i64,
+}
--- backend/src/auth/middleware.rs
+++ backend/src/auth/middleware.rs
@@ -1,12 +1,12 @@
 use crate::auth::jwt::validate_token;
 use crate::errors::ServiceError;
 use crate::state::AppState;
-use actix_web::{ Error, HttpMessage};
 use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
 use actix_web::http::header::{AUTHORIZATION, REFRESH};
 use actix_web::web::Data;
+use actix_web::{Error, HttpMessage};
 use futures::future::LocalBoxFuture;
-use std::future::{ready , Ready};
+use std::future::{Ready, ready};
 use std::rc::Rc;
 
 pub struct AuthMiddleware;
@@ -62,34 +62,46 @@ where
     }
 
     fn call(&self, mut req: ServiceRequest) -> Self::Future {
-      let srv = self.service.clone();
-      Box::pin(async move{
-        let app_state = match req.app_data::<Data<AppState>>() {
-            Some(d) => d.clone(),
-            None => return Err(actix_web::error::ErrorInternalServerError("app state error"))
-        }; 
-        let token_option = req.headers().get(AUTHORIZATION).and_then(|h| h.to_str().ok()).and_then(|s| {
-          let s = s.trim(); //removes the initial and trailing whitespaces  
-          if s.to_ascii_lowercase().starts_with("bearer "){
-            Some(s[7..].trim().to_string())
-          } else{
-            None
-          }
-        });
+        let srv = self.service.clone();
+        Box::pin(async move {
+            let app_state = match req.app_data::<Data<AppState>>() {
+                Some(d) => d.clone(),
+                None => {
+                    return Err(actix_web::error::ErrorInternalServerError(
+                        "app state error",
+                    ));
+                }
+            };
+            let token_option = req
+                .headers()
+                .get(AUTHORIZATION)
+                .and_then(|h| h.to_str().ok())
+                .and_then(|s| {
+                    let s = s.trim(); //removes the initial and trailing whitespaces  
+                    if s.to_ascii_lowercase().starts_with("bearer ") {
+                        Some(s[7..].trim().to_string())
+                    } else {
+                        None
+                    }
+                });
 
-        let token = match token_option {
-            Some(tk) => tk,
-            None => return Err(actix_web::error::ErrorUnauthorized("Missing Authorization header"))
-        }; 
+            let token = match token_option {
+                Some(tk) => tk,
+                None => {
+                    return Err(actix_web::error::ErrorUnauthorized(
+                        "Missing Authorization header",
+                    ));
+                }
+            };
 
-        let token_data = match validate_token(&token, &app_state.setting.jwt_secret){
-          Ok(tk) => tk,
-          Err(_) => return Err(actix_web::error::ErrorUnauthorized("Invalid Token"))
-        };
+            let token_data = match validate_token(&token, &app_state.setting.jwt_secret) {
+                Ok(tk) => tk,
+                Err(_) => return Err(actix_web::error::ErrorUnauthorized("Invalid Token")),
+            };
 
-        req.extensions_mut().insert(token_data.claims);
-        let response = srv.call(req).await?;
-        Ok(response)
-      })
+            req.extensions_mut().insert(token_data.claims);
+            let response = srv.call(req).await?;
+            Ok(response)
+        })
     }
 }
